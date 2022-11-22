52b03db348902b3a8d01ea4f562c80c2be6b2a76
yisas93 <juan249325@gmail.com>
2022-11-22 01:29:57 +0000

    domain

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let domainName = [];
+  let pro = ["the", "our", "new"];
+  let adj = ["great", "small", "awesome"];
+  let noun = ["peasant", "king", "fool"];
+  let dom = [".com", ".net", ".org"];
+  for (let a = 0; a < pro.length; a++) {
+    for (let b = 0; b < adj.length; b++) {
+      for (let c = 0; c < noun.length; c++) {
+        for (let d = 0; d < dom.length; d++) {
+          domainName.push("  " + pro[a] + adj[b] + noun[c] + dom[d]);
+        }
+      }
+    }
+  }
+  document.getElementById("domain").innerHTML = domainName;
 };
